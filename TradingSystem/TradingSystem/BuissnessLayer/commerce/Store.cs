ce04c7fa836b370279e087eb09d3f23aceb4912d
alikbader <alikbader@gmail.com>
2021-04-21 08:26:33 +0300

    Update Store.cs
    implemented basic method.
    changed the collections of managers and owners to contain strings; reason: while logging out and in again the Member objects will nit be updated which causes functionality confilcts.

--- TradingSystem/TradingSystem/BuissnessLayer/commerce/Store.cs
+++ TradingSystem/TradingSystem/BuissnessLayer/commerce/Store.cs
@@ -11,8 +11,8 @@ namespace TradingSystem.BuissnessLayer
         public string name { get; private set; }
         public ICollection<Reciept> recipts { get; private set; }
         public ICollection<Product> inventory { get; private set; }
-        public ICollection<aUser> owners { get; private set; }
-        public ICollection<aUser> managers { get; private set; }
+        public ICollection<string> owners { get; private set; }
+        public ICollection<string> managers { get; private set; }
         public Member founder { get; private set; }
 
         public Store(string name, Member founder)
@@ -21,6 +21,8 @@ namespace TradingSystem.BuissnessLayer
             this.founder = founder;
             this.recipts = new List<Reciept>();
             this.inventory = new List<Product>();
+            this.owners = new List<string>();
+            this.managers = new List<string>();
         }
 
         public double calcPrice(ICollection<Product> products)
@@ -34,36 +36,38 @@ namespace TradingSystem.BuissnessLayer
         }
         public void addOwner(string username)
         {
-
+            this.owners.Add(username);
         }
         public void addManager(string username)
         {
-
+            this.managers.Add(username);
         }
         public void removeOwner(string username)
         {
+            this.owners.Remove(username);
         }
 
         public void removeManager(string username)
         {
+            this.managers.Remove(username);
         }
 
         public bool isManager(string userrname)
         {
-            throw new NotImplementedException();
+            return this.managers.Contains(userrname);
         }
 
         public bool isOwner(string username)
         {
-            throw new NotImplementedException();
+            return this.owners.Contains(username);
         }
 
-        public ICollection<aUser> getOwners()
+        public ICollection<string> getOwners()
         {
             return owners;
         }
 
-        public ICollection<aUser> getManagers()
+        public ICollection<string> getManagers()
         {
             return managers;
         }
