018a05f9da8a098417aa423e312dff6bdeaa9a9d
shaulik12 <shaulik@post.bgu.ac.il>
2021-04-25 20:11:47 +0300

    service layer start

--- TradingSystem/TradingSystem/DataLayer/DataAccess/MemberDAL.cs
+++ TradingSystem/TradingSystem/DataLayer/DataAccess/MemberDAL.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace TradingSystem.DataLayer
 {
     static class MemberDAL
---
+++ TradingSystem/TradingSystem/ServiceLayer/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingSystem.ServiceLayer
+{
+    class UserController
+    {
+        public static bool login(string username, string password)
+        {
+            return BuissnessLayer.UserServices.login(username, password);
+        }
+
+        public static bool register(string username, string password)
+        {
+            return BuissnessLayer.UserServices.register(username, password);
+        }
+
+
+    }
+}
