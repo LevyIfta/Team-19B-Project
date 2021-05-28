1c776a43aac098590f51033c60ba6ec9fe8b6457
almogar <almogar@post.bgu.ac.il>
2021-05-28 18:18:55 +0300

    extSystem

---
+++ TradingSystem/PaymentSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PaymentSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TradingSystem/ProductDeliverySystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductDeliverySystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
