2870e91b1459a7ea0b7c730bd575b37d1b508b19
iftah <levyiftah@post.bgu.ac.il>
2021-05-30 19:24:55 +0300

    made policy skeleton

---
+++ TradingSystem/TradingSystem/BuissnessLayer/commerce/Rules/Policy/AndPolicy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingSystem.BuissnessLayer.commerce.Rules
+{
+    class AndPolicy : iPolicy
+    {
+        public iPolicy policy1 { get; set; }
+        public iPolicy policy2 { get; set; }
+
+        public AndPolicy(iPolicy pol1, iPolicy pol2)
+        {
+            this.policy1 = pol1;
+            this.policy2 = pol2;
+        }
+
+        public bool isValid(ICollection<Product> products)
+        {
+            return this.policy1.isValid(products) && this.policy2.isValid(products);
+        }
+    }
+}
